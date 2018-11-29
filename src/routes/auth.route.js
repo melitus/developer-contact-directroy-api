6338047c079c8b0197898d18d5c6fe51e46c174d
melitus <asmelitus@gmail.com>
2018-11-29 13:59:44 +0100

    created auth route

---
+++ src/routes/auth.route.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const validate = require('express-validation');
+
+const authController = require('../controllers/auth.controller');
+const oAuthLogin = require('../middlewares/auth').oAuth;
+const {
+  login,
+  register,
+  oAuth,
+  refresh,
+} = require('../../validations/auth.validation');
+
+const router = express.Router();
+
+// [POST] auth/register Register
+router.route('/register')
+  .post(validate(register), authController.register);
+
+// [POST] auth/login Login
+router.route('/login')
+  .post(validate(login), authController.login);
+
+
+// [POST] auth/refresh-token Refresh Token
+router.route('/refresh-token')
+  .post(validate(refresh), authController.refresh);
+
+
+// [POST] auth/reset-password
+ 
+
+
+// [POST] auth/facebook Facebook Login
+router.route('/facebook')
+  .post(validate(oAuth), oAuthLogin('facebook'), authController.oAuth);
+
+// [POST] auth/google Google Login
+router.route('/google')
+  .post(validate(oAuth), oAuthLogin('google'), authController.oAuth);
+
+
+module.exports = router;
